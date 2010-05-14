43a6c55ee72db2b161e3c4c9dfd272f177166710
Denis Bazhenov <bazhenov@farpost.com>
2010-05-15 10:21:35 +1100

    debian package build script added

---
+++ create-debian-package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TMP=/tmp/load-test-tool-1.0-1
+
+# Prepare build environment
+rm -rf $TMP
+mkdir -p $TMP/usr/share
+
+# Copying program files
+tar -C $TMP/usr/share -xf ./ldt-assembly/target/ldt-assembly-1.0-SNAPSHOT.tar.gz
+mv $TMP/usr/share/ldt-assembly-1.0-SNAPSHOT $TMP/usr/share/load-test-tool
+
+# Copying debian manifest
+mkdir -p $TMP/DEBIAN
+cp debian/control $TMP/DEBIAN
+
+# Building package
+dpkg-deb -b $TMP
+mv $TMP.deb .
