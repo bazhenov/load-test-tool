6e7dec68ba4e1a570a7bff3e1fe0f59bd7420db6
Denis Bazhenov <bazhenov@farpost.com>
2010-07-28 11:34:18 +1100

    fixed: ldt.sh rewrite LDT_CLASSPATH environment variable

--- ldt-assembly/assembly/bin/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LINK=$0
-[ -h $0 ] && LINK=`readlink $0`
-WHOME=`dirname $LINK`
-
-ln -sf $WHOME/ldt.sh /usr/bin/ldt
-chmod +x /usr/bin/ldt
--- ldt-assembly/assembly/bin/ldt.sh
+++ ldt-assembly/assembly/bin/ldt.sh
@@ -3,6 +3,6 @@
 LINK=$0
 [ -h $0 ] && LINK=`readlink $0`
 WHOME=`dirname $LINK`
-LDT_CLASSPATH=.:./target/classes
+LDT_CLASSPATH=.:./target/classes:$LDT_CLASSPATH
 
 java $JAVA_OPTS -cp "$WHOME/../lib/*:$LDT_CLASSPATH" com.farpost.ldt.Main $@
