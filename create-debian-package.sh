143c720a3a80aa6ecc1837bff90b1e777a66fd28
Denis Bazhenov <bazhenov@farpost.com>
2011-03-26 00:06:24 +1000

    Debian script modified accordingly to a new ldt-assembly version

--- create-debian-package.sh
+++ create-debian-package.sh
@@ -7,8 +7,8 @@ rm -rf $TMP
 mkdir -p $TMP/usr/share
 
 # Copying program files
-tar -C $TMP/usr/share -xf ./ldt-assembly/target/ldt-assembly-1.0-SNAPSHOT.tar.gz
-mv $TMP/usr/share/ldt-assembly-1.0-SNAPSHOT $TMP/usr/share/load-test-tool
+tar -C $TMP/usr/share -xf ./ldt-assembly/target/ldt-assembly-1.1.tar.gz
+mv $TMP/usr/share/ldt-assembly-1.1 $TMP/usr/share/load-test-tool
 
 # Copying debian manifest
 mkdir -p $TMP/DEBIAN
